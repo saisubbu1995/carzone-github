234fa88a7ac4ef0f47a948aba04b8c4dd9c963ca
Subrahmanyam <subrahmanyampkdm@gmail.com>
2024-07-02 17:00:03 +0530

    Insertion of cars into database and accessing them to frontend

--- carsapp/admin.py
+++ carsapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import CarsModel
+from django.utils.html import format_html
 
 # Register your models here.
+class CarAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src= "{}" width="40" style="border-radius: 50px;"/>'.format(object.car_photo.url))
+    
+    thumbnail.short_description = 'Photo'
+
+    list_display = ('id', 'thumbnail', 'car_title', 'color', 'model', 'condition', 'fuel_type', 'is_featured',)
+    list_display_links = ('id', 'thumbnail', 'car_title',)
+    list_editable = ('is_featured',)
+    search_fields = ('id', 'car_title', 'city', 'model', 'fuel_type')
+    list_filter = ('car_title', 'city', 'model', 'fuel_type',)
+
+
+admin.site.register(CarsModel, CarAdmin)

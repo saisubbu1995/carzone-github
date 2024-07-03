e0f56ca4cbbba100e303b0cfa099c57672fcb0ea
Subrahmanyam <subrahmanyampkdm@gmail.com>
2024-07-03 14:59:47 +0530

    Cars page setup and made it in working condition

--- carsapp/views.py
+++ carsapp/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import CarsModel
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def cars(request):
-    return render(request, 'cars\cars.html')
+    all_cars = CarsModel.objects.order_by('-created_date')
+    paginator = Paginator(all_cars, 2)
+    page = request.GET.get('page')
+    paged_cars = paginator.get_page(page)
+    data = {
+        'all_cars': paged_cars,
+    }
+    return render(request, 'cars\cars.html', data)
+
+def car_details(request, id):
+    single_car = get_object_or_404(CarsModel, pk=id)
+    print(single_car.car_title, single_car.city)
+    data = {
+        'single_car': single_car,
+    }
+    return render(request, 'cars\car_details.html', data)
